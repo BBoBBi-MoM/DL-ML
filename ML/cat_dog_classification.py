ef6807284f18340236cf2ae1fea25ea647c07beb
BBoBBi-MoM <shanghai110930@gamil.com>
2023-01-14 13:23:48 +0900

    모델 저장

--- ML/cat_dog_classification.py
+++ ML/cat_dog_classification.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 import torchvision
 from torch.utils.data import DataLoader
+import pandas as pd
 
 #%% hyper parameters
 
@@ -18,9 +19,9 @@ transform = transforms.Compose([transforms.Resize((128,128)),
                                 transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
                                 ])
 
-train_set = torchvision.datasets.ImageFolder(root=r'C:\Users\Administrator\Desktop\python_1\DL-ML\dataset\cat_dog\training_set',
+train_set = torchvision.datasets.ImageFolder(root=r'C:\Users\Administrator\Desktop\python\DL-ML\ML\dataset\training_set',
                                              transform = transform)
-test_set = torchvision.datasets.ImageFolder(root=r'C:\Users\Administrator\Desktop\python_1\DL-ML\dataset\cat_dog\test_set',
+test_set = torchvision.datasets.ImageFolder(root=r'C:\Users\Administrator\Desktop\python\DL-ML\ML\dataset\test_set',
                                             transform = transform)
 
 data_loader = DataLoader(dataset=train_set,batch_size=batch_size,shuffle=True,drop_last=True)
@@ -69,15 +70,16 @@ class CNN(nn.Module):
         output = self.fc1(output)
         output = self.fc2(output)
         return output
-
+#%%
 model = CNN()
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(),lr=learning_rate)
-
+#%%
 total_batch = len(data_loader)
 print('Learnig start')
 for epoch in range(traing_epoch):
-    average_cost = 0
+    average_cost = 0.0
+
     for num,data in enumerate(data_loader):
         imgs ,labels = data
         optimizer.zero_grad()
@@ -85,11 +87,47 @@ for epoch in range(traing_epoch):
         loss = criterion(out,labels)
         loss.backward()
         optimizer.step()
-
-        
+        _,argmax =  torch.max(out,1)
+        accuracy = (argmax==labels).float().mean()
         average_cost += loss / total_batch
-        break
-    print('[Epoch:{}] cost = {}'.format(epoch+1,average_cost))
+    print('[EPOCH:{}/{}] COST:{:.5f} , ACCURACY:{}%'.format(epoch+1,traing_epoch,average_cost,(accuracy*100)))
     
 print('finished')
+#%%
+
+
+# %% save the model
+SAVE_PATH = r'C:\Users\Administrator\Desktop\python\DL-ML\ML\saved_model\cat_dog_0114_01.pt'
+torch.save(model, SAVE_PATH)
+#%% Load the model
+LOAD_PATH = r'C:\Users\Administrator\Desktop\python\DL-ML\ML\saved_model\cat_dog_0114_01.pt'
+model = torch.load(SAVE_PATH)
+#%% confusion matrix
+column_list = ['cat img','dog img']
+row_list = ['cat pred','dog pred']
+val = [[0,0],[0,0]]
+table = pd.DataFrame(columns=column_list,index=row_list)
+table.fillna(0,inplace=True)
+#%% Test
+test_loader = DataLoader(dataset = test_set, batch_size=1)
+with torch.no_grad():
+    correct_count = 0
+    for num,data in enumerate(test_loader):
+        img , label = data
+        prediction = model(img)
+        _,argmax = torch.max(prediction,1)
+        
+        if label ==0:            #사진이 고양이일때
+            if argmax== 0:        #예측도 고양이일때
+                table.iloc[0,0] +=1
+            else:                #예측은 개일때
+                 table.iloc[1,0]+=1
+        else :                    #사진이 개일때
+            if argmax==0:         #예측은 고양이일때
+                table.iloc[0,1]+=1
+            else:
+                table.iloc[1,1]+=1
+        
+    print(table)
+    print(f'ACCURACY:{(table.iloc[0,0]+table.iloc[1,1])/len(test_loader)*100}%')
 # %%
