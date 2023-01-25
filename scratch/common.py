dc39e8ae93c9aa3ff612b8773579d555ebb3c611
BBoBBi-MoM <shanghai110930@gamil.com>
2023-01-26 08:08:28 +0900

    .

--- scratch/common.py
+++ scratch/common.py
@@ -94,3 +94,56 @@ Z1 = sigmoid(A1)
 # %%
 Z1
 # %%
+W2 = np.array([[0.1,0.4],[0.2,0.5],[0.3,0.6]])
+B2 = np.array([0.1,0.2])
+
+print(Z1.shape)
+print(W2.shape)
+print(B2.shape)
+A2 = np.dot(Z1,W2)+B2
+Z2 = sigmoid(A2)
+# %%
+def identify_function(x):
+    return x
+#%%
+W3 = np.array([[0.1,0.3],[0.2,0.4]])
+B3 = np.array([0.1,0.2])
+A3 = np.dot(Z2,W3)+B3
+# %%
+Y = identify_function(A3)
+# %%
+def init_networt():
+    network = dict()
+    network['W1'] = np.array([[0.1,0.3,0.5],[0.2,0.4,0.6]])
+    network['b1'] = np.array([0.1,0.2,0.3])
+    network['W2'] = np.array([[0.1,0.4],[0.2,0.5],[0.3,0.6]])
+    network['b2'] = np.array([0.1,0.2])
+    network['W3'] = np.array([[0.1,0.3],[0.2,0.4]])
+    network['b3'] = np.array([0.1,0.2])
+
+    return network
+def forward(network,x):
+    W1,W2,W3 = network['W1'],network['W2'],network['W3']
+    b1,b2,b3 = network['b1'],network['b2'],network['b3']
+
+    a1 = np.dot(x,W1)+b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1,W2)+b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2,W3)+b3
+    y = identify_function(a3)
+    return y
+#%%
+network = init_networt()
+x = np.array([1.,0.5])
+y = forward(network,x)
+# %%
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a-c)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a/sum_exp_a
+    
+    return y
+
+# %%
